c4ed11456c44c188d2484524e35ab6b4021632d0
RamaSai2519 <rchandik@gitam.in>
2024-11-11 00:03:49 +0530

    Remove kill_process_on_port function to simplify process management

--- supervisor_restarter.py
+++ supervisor_restarter.py
@@ -19,23 +19,8 @@ def start_supervisor_process(process_name):
     run_subprocess(['supervisorctl', 'start', process_name])
 
 
-def kill_process_on_port(port):
-    try:
-        lsof_command = subprocess.run(
-            ['lsof', '-ti', f':{port}'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        pids = lsof_command.stdout.decode().strip().split()
-        if pids:
-            run_subprocess(['xargs', 'kill', '-9'],
-                           input='\n'.join(pids).encode())
-        else:
-            print(f"No process found running on port {port}")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e.stderr.decode()}")
-
-
 if __name__ == "__main__":
     stop_all_supervisors()
-    # kill_process_on_port('8080')
     start_supervisor_process("mark")
     time.sleep(5)
     start_supervisor_process("markb")
