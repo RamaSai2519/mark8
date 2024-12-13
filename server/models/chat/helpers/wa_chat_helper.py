a08b2b962e3cb8dafaa81e0ccd3082eb52ac32a3
RamaSai2519 <rchandik@gitam.in>
2024-12-13 15:45:08 +0530

    Update server configuration and enhance WaChatHelper with user details handling

--- server/models/chat/helpers/wa_chat_helper.py
+++ server/models/chat/helpers/wa_chat_helper.py
@@ -3,16 +3,17 @@ import openai
 import requests
 from .args_schemas import *
 from shared.models.common import Common
-from datetime import datetime, timedelta
 from shared.models.interfaces import Output
 from shared.configs import CONFIG as config
 from shared.models.constants import TimeFormats
+from shared.db.chat import get_system_prompts_collection
 
 
 class WaChatHelper:
     def __init__(self, phoneNumber: str) -> None:
         self.context = 'wa_webhook'
         self.phoneNumber = phoneNumber
+        self.system_prompts_collection = get_system_prompts_collection()
 
     def get_available_sarathis(self, arguments: dict = None) -> list:
         url = config.URL + '/actions/expert'
@@ -48,8 +49,9 @@ class WaChatHelper:
 
         return {
             'name': user.get('name', ''),
-            'persona': user.get('customerPersona', ''),
-            'user_registered': True if user.get('profileCompleted') == True else False
+            'city': user.get('city', ''),
+            'birthDate': user.get('birthDate', '').strftime('%Y-%m-%d') if user.get('birthDate') else '',
+            'persona': user.get('customerPersona', '')
         }
 
     def register_user(self, name: str = None, city: str = None, birthDate: str = None) -> dict:
@@ -72,7 +74,7 @@ class WaChatHelper:
             openai.pydantic_function_tool(
                 GetUpcomingEvents, description="Get the list of upcoming events. Call this whenever the user wants to know about the upcoming events. It returns a list of events with their main title, sub title, hosted by, guest speaker, event start date time, event type, prize money, event price, and event registration link. Do not return the list as it is or all at once."),
             openai.pydantic_function_tool(
-                GetUserDetails, description="Get the user details. Call this whenever the user wants to know about their profile details. It returns the user's name, persona, and whether the user is registered or not."),
+                GetUserDetails, description="Get the user details. Call this whenever you want to know about the user's profile details. It returns the user's name, persona, city and birthDate. Use this to check if the user is registered or not."),
             openai.pydantic_function_tool(
                 SaveUserCity, description="Save the user's city."),
             openai.pydantic_function_tool(
@@ -104,7 +106,7 @@ class WaChatHelper:
         return current_time.strftime('%Y-%m-%d %H:%M:%S')
 
     def get_system_message(self) -> str:
-        return f"""
+        prompt = f"""
         You are a customer service chatbot for 'Sukoon Unlimited'(called 'Sukoon' at times), a company dedicated to enriching the lives of senior citizens by fostering meaningful connections,
         emotional well-being, and community engagement. Sukoon Unlimited provides conversation-based activities, therapist-led support groups, and expert advice in
         areas like mental health, financial planning, and spirituality. Sukoon Sarathis are mentors or facilitators associated with Sukoon Unlimited. They are individuals
@@ -136,10 +138,16 @@ class WaChatHelper:
         And you will only describe them briefly and answer further queries if asked by the user.
         You will not share entire list at once, but share few events by relevance of time and few sarathis by their personas.
 
-        Remember to always check if the user is registered or not and nudging them to register if they are not.
-        If user is not registered, you can guide them to register by providing their name, city, and birthdate. 
+        Name, City, and Birthdate are the details that the user is to provide to register on the platform.
+        If any of these details are missing, you can ask the user to provide them once at the start of the conversation and once at the end.
         The user is marked as registered only when all the details are provided but remember to save whatever details are provided by the user immediately.
+        Also make sure to capture these details when user is talking about them in any context.
+        Do not ask for these details again if we already have them. You can use the `GetUserDetails` function to check if the user is registered or not.
 
+        - Use the `GetUserDetails` function to check if the user is registered or not. The function returns the user's name, persona, and whether the user is registered or not.
+        - Always greet the user with their name if they are registered else ask them to register by providing their name, city, and birthdate.
+        - Remember to greet the user with a brief introduction and a overview of your capabilities.
+        - One of your main objective is to register the user on the platform by asking for their name, city, and birthdate.
         - Call the appropriate function to fetch specific information like Available Sarathis, Upcoming Events, or User Details.
         - While dealing with date strings when you want to call functions, always use this format: {TimeFormats.ANTD_TIME_FORMAT}.
         - Try to converse in the language user is speaking in.
@@ -158,4 +166,13 @@ class WaChatHelper:
         You are only to converse and help the user with the queries related to the platform and the services provided by Sukoon Unlimited and nothing else.
         Make sure to keep the conversation engaging and informative. Remember to use emojis whenever necessary to make the conversation more engaging and friendly.
         Keep the message clean and format it properly before sending it to the user. Try to use bullet points and paragraphs to make the message more readable.
+        Keep the message as detailed and informative as possible but do not share any information that is not provided by the user or the system.
         """
+
+        system_prompt = self.system_prompts_collection.find_one(
+            {'context': self.context})
+        if system_prompt:
+            prompt += "\n Here's everything you need to know about Sukoon Unlmited:\n"
+            prompt += system_prompt['content']
+        prompt += "\n\n ONLY SHARE AND ANSWER THE QUESTIONS ABOUT THE INFORMATION YOU HAVE BEEN PROVIDED WITH. DO NOT SHARE ANY OTHER INFORMATION OR ENGAGE IN ANY OTHER CONVERSATION WITH THE USER. DO NOT ASSUME ANYTHING THAT YOU DON'T KNOW ABOUT THE COMPANY OR THE SERVICES PROVIDED BY THE COMPANY. IF YOU ARE UNSURE ABOUT ANYTHING, PLEASE DIRECT THE USER TO THE SUPPORT TEAM. \n\n"
+        return prompt
